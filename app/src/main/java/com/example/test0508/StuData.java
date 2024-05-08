45f73ff87f0e8808e106685f50d0b64eb0edb4a0
Hank <111b15005@mailst.cjcu.edu.tw>
2024-05-08 16:19:32 +0800

    stuData排版

--- app/src/main/java/com/example/test0508/StuData.java
+++ app/src/main/java/com/example/test0508/StuData.java
@@ -5,26 +5,32 @@ public class StuData {
     private String height;
 
     public String getId() {
+
         return id;
     }
 
     public void setId(String id) {
+
         this.id = id;
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public String getHeight() {
+
         return height;
     }
 
     public void setHeight(String height) {
+
         this.height = height;
     }
 
