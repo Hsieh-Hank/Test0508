a0a94d5759d33ce33f4e8c6fb74f36aa467253bb
Hank <111b15005@mailst.cjcu.edu.tw>
2024-05-15 16:12:41 +0800

    未完成

--- app/src/main/java/com/example/test0508/MainActivity.java
+++ app/src/main/java/com/example/test0508/MainActivity.java
@@ -1,26 +1,33 @@
 package com.example.test0508;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//https://github.com/cflin-cjcu/Test0508
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private List<StuData> stuDataList;
+    private StuDataAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView recyclerView = findViewById(R.id.rvMyData);
+        recyclerView = findViewById(R.id.rvMyData);
+
+        stuDataList = new ArrayList<>();
 
         List<StuData> stuDataList = new ArrayList<>();
         stuDataList.add(new StuData("https://www.apple.com/newsroom/images/logos/quick-reads-logos/Apple-environment-logo.jpg.square_social.jpg", "John", "180"));
-        stuDataList.add(new StuData("https://www.apple.com/newsroom/images/logos/quick-reads-logos/Apple-environment-logo.jpg.square_social.jpg", "Tom", "175"));
+        stuDataList.add(new StuData("https://www.iconpacks.net/icons/2/free-apple-icon-3155-thumb.png", "Tom", "175"));
         stuDataList.add(new StuData("https://www.apple.com/newsroom/images/logos/quick-reads-logos/Apple-environment-logo.jpg.square_social.jpg", "Jerry", "170"));
         stuDataList.add(new StuData("https://www.apple.com/newsroom/images/logos/quick-reads-logos/Apple-environment-logo.jpg.square_social.jpg", "Mike", "165"));
         stuDataList.add(new StuData("https://www.apple.com/newsroom/images/logos/quick-reads-logos/Apple-environment-logo.jpg.square_social.jpg", "Jack", "160"));
@@ -29,9 +36,27 @@ public class MainActivity extends AppCompatActivity {
         stuDataList.add(new StuData("https://www.apple.com/newsroom/images/logos/quick-reads-logos/Apple-environment-logo.jpg.square_social.jpg", "Lucy", "145"));
         stuDataList.add(new StuData("https://www.apple.com/newsroom/images/logos/quick-reads-logos/Apple-environment-logo.jpg.square_social.jpg", "Bob", "140"));
         stuDataList.add(new StuData("https://www.apple.com/newsroom/images/logos/quick-reads-logos/Apple-environment-logo.jpg.square_social.jpg", "Alice", "135"));
-
-        recyclerView.setAdapter(new StuDataAdapter(stuDataList));
+        adapter = new StuDataAdapter(stuDataList);
+        recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+    }
+
+    public void addData(View view) {
+        Intent intent = new Intent(this, AddDataActivity.class);
+        startActivityForResult(intent, 1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            String name = data.getStringExtra("name");
+            String height = data.getStringExtra("height");
+            String url = data.getStringExtra("url");
+            stuDataList.add(new StuData(url, name, height));
+            adapter.notifyDataSetChanged();
+        }
     }
 }
--- app/src/main/java/com/example/test0508/StuDataAdapter.java
+++ app/src/main/java/com/example/test0508/StuDataAdapter.java
@@ -12,28 +12,25 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 
-public class StuDataAdapter extends RecyclerView.Adapter<StuDataAdapter.ViewHolder>{
+class StuDataAdapter extends RecyclerView.Adapter<StuDataAdapter.ViewHolder> {
+
     private ImageView img;
-    private  TextView tvname1;
-    private TextView tvheight1;
+    private TextView tvName1;
+    private TextView tvHeight1;
+
+    public List<StuData> getStuDataList() {
+        return stuDataList;
+    }
+
     private List<StuData> stuDataList;
 
     public StuDataAdapter(List<StuData> stuDataList) {
         this.stuDataList = stuDataList;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            img = itemView.findViewById(R.id.tvPic);
-            tvname1 = itemView.findViewById(R.id.tvName);
-            tvheight1 = itemView.findViewById(R.id.tvHeight);
-        }
-    }
-
     @NonNull
     @Override
-    public StuDataAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         Context context = parent.getContext();
         View view = View.inflate(context, R.layout.activity_my_data_view, null);
         ViewHolder viewHolder = new ViewHolder(view);
@@ -41,16 +38,26 @@ public class StuDataAdapter extends RecyclerView.Adapter<StuDataAdapter.ViewHold
     }
 
     @Override
-    public void onBindViewHolder(@NonNull StuDataAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         StuData stuData = stuDataList.get(position);
 //        img.setImageResource(stuData.getId());
-        Glide.with(holder.itemView).load(stuData.getImageURL()).into(img);
-        tvname1.setText(stuData.getName());
-        tvheight1.setText(stuData.getHeight());
+        Glide.with(holder.itemView).load(stuData.getImageUrl()).into(img);
+        tvHeight1.setText(stuData.getHeight());
+        tvName1.setText(stuData.getName());
+
     }
 
     @Override
     public int getItemCount() {
         return stuDataList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+            img = itemView.findViewById(R.id.tvPic);
+            tvName1 = itemView.findViewById(R.id.tvName);
+            tvHeight1 = itemView.findViewById(R.id.tvHeight);
+        }
     }
 }
